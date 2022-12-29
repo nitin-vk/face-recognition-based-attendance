1471c909b0a6a51a5ae53b397df4844f33917782
nitin-vk <nitinvkavya@gmail.com>
2022-12-29 09:09:20 +0530

    created uid for client side

--- FacesRecognition.py
+++ FacesRecognition.py
@@ -2,65 +2,118 @@ import os
 import numpy as np
 import cv2 as cv
 from FaceDetectionModule import FaceDetectionModule
-dir=r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\Faces\train"
-l=30
-t=7
-haar_cascade = cv.CascadeClassifier(r'C:\Users\Nitin V Kavya\Desktop\python\OpenCV\haar_face.xml')
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtWidgets import QFileDialog,QMessageBox
+import sys
 
-#people = ['Ben Afflek', 'Elton John', 'Jerry Seinfield', 'Madonna', 'Mindy Kaling', 'Nitin']
-people = []
-faces_read={}
-for i in os.listdir(dir):
-    people.append(i)
-print(people)
-# features = np.load('features.npy', allow_pickle=True)
-# labels = np.load('labels.npy')
+class FacesRecognition(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(FacesRecognition, self).__init__()
+        uic.loadUi('FacesRecognition.ui', self)
+        self.show()
+        self.yml_file=''
+        self.haar_cascade=''
+        self.ftp_dir=''
+        self.afterSelectFrame.hide()
+        self.downloadBtn.hide()
+        self.recognizeBtn.hide()
+        self.frame_4.hide()
+        self.frame_5.hide()
+        self.videoTypeWindow.hide()
+        self.localBtn.clicked.connect(self.serachLocalFiles)
+        self.recognizeBtn.clicked.connect(self.recognizeFaces)
+        self.cancelBtn.clicked.connect(self.hideVideoTypeWindow)
+        self.okBtn.clicked.connect(self.startStreaming)
 
-face_recognizer = cv.face.LBPHFaceRecognizer_create()
-face_recognizer.read('face_trained.yml')
 
-capture=cv.VideoCapture(0)
-f=FaceDetectionModule()
-while True:
+    def serachLocalFiles(self):
+        self.yml_file=QFileDialog.getOpenFileName(self, 'Open file', 
+        'c:\\',"YML files (*.yml)")
+        self.yml_file=str(self.yml_file)
+        pos=self.yml_file.index(',')
+        self.yml_file=self.yml_file[2:pos-1]
+        pos=self.yml_file.rindex('/')
+        yml_file=self.yml_file[pos+1:len(self.yml_file)+1]
+        #print(self.yml_file)
+        if self.yml_file!='':
+            self.fileLocation.insertPlainText(yml_file)
+        self.afterSelectFrame.show()
+        self.downloadBtn.show()
+        self.downloadBtn.setEnabled(False)
+        self.recognizeBtn.show()
+
+    def recognizeFaces(self):
+        self.videoTypeWindow.show()
+        self.frame_4.show()
+        self.frame_5.show()
+
+    def hideVideoTypeWindow(self):
+        self.frame_4.hide()
+        self.frame_5.hide()
+        self.videoTypeWindow.hide()
+
+    def startStreaming(self):
+        videoType=''
+        if self.liveStreamBtn.isChecked():
+            videoType=0
+        elif self.capturedVideoBtn.isChecked():
+            videoType=QFileDialog.getOpenFileName(self, 'Open file', 
+            'c:\\',"VIDEO FILES (*.avi)")
+            videoType=str(videoType)
+            pos=videoType.index(',')
+            videoType=videoType[2:pos-1]
+        else:
+              QMessageBox.about(self, "ERROR", "SELECT THE TYPE OF VIDEO")
+              return
+        print("videotype is {}".format(videoType))
+        dir=r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\Faces\train"
+        l=30
+        t=7
+        haar_cascade = cv.CascadeClassifier(r'C:\Users\Nitin V Kavya\Desktop\python\OpenCV\haar_face.xml')
+        people = []
+        faces_read={}
+        for i in os.listdir(dir):
+            people.append(i)
+        print(people)
+
+
+        face_recognizer = cv.face.LBPHFaceRecognizer_create()
+        face_recognizer.read(self.yml_file)
+
+        capture=cv.VideoCapture(videoType)
+        f=FaceDetectionModule()
+        while True:
     
-    isTrue,img=capture.read()
-#img = cv.imread(r"C:\Users\Nitin V Kavya\Desktop\python\OpenCV\Faces\val\elton_john\3.jpg")
-    img,boxes=f.findFace(img)
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    #cv.imshow('Person', gray)
+            isTrue,img=capture.read()
 
-# Detect the face in the image
-    faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+            img,boxes=f.findFace(img)
+            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    
 
-    for (x,y,w,h) in faces_rect:
+
+            faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
+
+            for (x,y,w,h) in faces_rect:
         
-        x1,y1=x+w,y+h
-        faces_roi = gray[y:y+h,x:x+w]
+                x1,y1=x+w,y+h
+                faces_roi = gray[y:y+h,x:x+w]
 
-        label, confidence = face_recognizer.predict(faces_roi)
-        print(f'Label = {people[label]} with a confidence of {confidence}')
+                label, confidence = face_recognizer.predict(faces_roi)
+                print(f'Label = {people[label]} with a confidence of {confidence}')
 
-        cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
-        faces_read.setdefault(people[label],[])
-        faces_read[people[label]].append(confidence)
+                cv.putText(img, str(people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
+                faces_read.setdefault(people[label],[])
+                faces_read[people[label]].append(confidence)
         
-       
+            cv.imshow('Detected Face', img)
+            if cv.waitKey(20) & 0xFF==ord('b'):
+                break
+
+        capture.release()
+        cv.destroyAllWindows()
+
+app = QtWidgets.QApplication(sys.argv)
+window = FacesRecognition()
+app.exec_()
 
 
-        
-
-
-
-    cv.imshow('Detected Face', img)
-    if cv.waitKey(20) & 0xFF==ord('b'):
-        break
-
-capture.release()
-cv.destroyAllWindows()
-print(faces_read)
-for i in faces_read.keys():
-    faces_read[i].sort()
-
-    if len(faces_read[i])>10:
-        faces_read[i]=faces_read[i][0:10]
-print(faces_read)
