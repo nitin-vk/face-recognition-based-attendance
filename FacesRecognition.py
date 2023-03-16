60fc3a360d3fc9f0aaae592da75bf39fcd7b64a1
Nitin V Kavya <nitinvkavya@gmail.com>
2023-03-16 14:44:14 +0530

    restored LBPH args

--- FacesRecognition.py
+++ FacesRecognition.py
@@ -158,7 +158,7 @@ class FacesRecognition(QtWidgets.QMainWindow):
         print(self.people)
 
 
-        face_recognizer = cv.face.LBPHFaceRecognizer_create()
+        face_recognizer = cv.face.LBPHFaceRecognizer_create(1.1,3)
         face_recognizer.read(self.yml_file)
 
         capture=cv.VideoCapture(videoType)
