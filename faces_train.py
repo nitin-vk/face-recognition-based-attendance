b242979c89f12f85a59ba94404c65771b3eee424
Nitin V Kavya <nitinvkavya@gmail.com>
2023-03-17 08:09:55 +0530

    used face_recognition module

--- faces_train.py
+++ faces_train.py
@@ -1,17 +1,28 @@
 import os
 import cv2 as cv
 import numpy as np
+import face_recognition
+import pickle
 dir=r"D:\Faces\8CSEB"
 people=[]
-haar_cascade=cv.CascadeClassifier(r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\haar_cascade_files\data\haarcascades\haarcascade_frontalcatface.xml")
+#haar_cascade=cv.CascadeClassifier(r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\haar_cascade_files\data\haarcascades\haarcascade_frontalcatface.xml")
 for i in os.listdir(dir):
     people.append(i)
-print(people)
-features=[]
-labels=[]
+#print(people)
+#features=[]
+#labels=[]
 
 def create_train():
-    for person in people:
+    known_face_encodings = []
+    known_face_names=[]
+    for i in os.listdir(dir):
+        for file in os.listdir(os.path.join(dir,i)):
+            image = face_recognition.load_image_file(os.path.join(dir,file))
+            face_encoding = face_recognition.face_encodings(image)[0]
+            known_face_encodings.append(face_encoding)
+    with open("encodings.txt", "wb") as fp:
+        pickle.dump((known_face_encodings, known_face_names), fp)
+    '''for person in people:
         path=os.path.join(dir,person)
         label=people.index(person)
 
@@ -40,4 +51,4 @@ face.train(features,labels)
 
 face.save('face_trained.yml')
 np.save('features.npy', features)
-np.save('labels.npy', labels)
+np.save('labels.npy', labels)'''
