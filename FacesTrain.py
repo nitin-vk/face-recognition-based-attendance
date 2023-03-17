778ed3a55eaaf35b5ee0be0a320a6abcacfb0044
Nitin V Kavya <nitinvkavya@gmail.com>
2023-03-17 13:33:16 +0530

    removed select xml file

--- FacesTrain.py
+++ FacesTrain.py
@@ -18,22 +18,12 @@ class FacesTrain(QtWidgets.QMainWindow):
         self.dir=''
         self.haar_cascade=''
         self.ftp_dir=''
-        self.selectXml.clicked.connect(self.selectXmlFile)
         self.selectDir.clicked.connect(self.selectDirectory)
         self.trainBtn.clicked.connect(self.trainFaces)
         self.progressFrame.hide()
         self.doneFrame.hide()
 
-    def selectXmlFile(self):
-        self.haar_cascade = QFileDialog.getOpenFileName(self, 'Open file', 
-        'c:\\',"XML files (*.xml)")
-        #print(str(self.haar_cascade))
-        self.haar_cascade=str(self.haar_cascade)
-        pos=self.haar_cascade.index(',')
-        self.haar_cascade=self.haar_cascade[2:pos-1]
-        #print(os.path.exists(str(self.haar_cascade)))
-        
-
+    
     def selectDirectory(self):
         self.dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
         self.ftp_dir=str(self.dir[self.dir.rindex('/')+1:len(self.dir)])
@@ -44,9 +34,7 @@ class FacesTrain(QtWidgets.QMainWindow):
             print("Select the training directory")
             return
 
-        '''if self.haar_cascade=='':
-            print("select the XML file for haar cascading")
-            return'''
+       
         
         self.progressFrame.show()
         dir=self.dir
@@ -55,7 +43,6 @@ class FacesTrain(QtWidgets.QMainWindow):
         for i in os.listdir(dir):
             known_face_names.append(i)
         for i in os.listdir(dir):
-            print(int((known_face_names.index(i)+1)/(len(known_face_names))*100))
             self.trainProgress.setValue(int((int(known_face_names.index(i))+1)/(len(known_face_names))*100))
             self.trainProgress.setTextVisible(False)
             for file in os.listdir(os.path.join(dir,i)):
@@ -68,53 +55,7 @@ class FacesTrain(QtWidgets.QMainWindow):
         with open((os.path.join('D:/Compiled Files/',folderName,'encodings.txt')), "wb") as fp:
             pickle.dump((known_face_encodings, known_face_names), fp)
         self.doneFrame.show()
-        '''dir=self.dir
-        people=[]
-        print("from train {}".format(self.haar_cascade))
-        haar_cascade=cv.CascadeClassifier(self.haar_cascade)
-        for i in os.listdir(dir):
-            people.append(i)
-        print(people)
-        features=[]
-        labels=[]
-        self.create_train(dir,people,features,labels,haar_cascade)
-        #print("Training Done")
-        self.doneFrame.show()
-        features=np.array(features,dtype='object')
-        labels=np.array(labels)
-        face=cv.face.LBPHFaceRecognizer_create(radius=1,neighbors=3)
-        face.train(features,labels)
-        folderName=self.ftp_dir
-        if os.path.exists('D:/Compiled Files/'+folderName)==False:
-            os.mkdir('D:/Compiled Files/'+folderName)
         
-
-        face.save('D:/Compiled Files/'+folderName+'/compiled.yml')
-        np.save('D:/Compiled Files/'+folderName+'/features', features)
-        np.save('D:/Compiled Files/'+folderName+'/labels', labels)'''
-
-          
-
-    '''def create_train(self,dir,people,features,labels,haar_cascade):
-        for person in people:
-            path=os.path.join(str(dir),person)
-            label=people.index(person)
-            self.trainProgress.setValue(int((label+1)/(len(people))*100))
-            self.trainProgress.setTextVisible(False)
-            #time.sleep(5)
-            for img in os.listdir(path):
-                img_path=os.path.join(path,img)
-                img_array=cv.imread(img_path)
-                if img_array is None:
-                    continue
-        
-
-                gray=cv.cvtColor(img_array,cv.COLOR_BGR2GRAY)
-                face_rect=haar_cascade.detectMultiScale(gray,scaleFactor=1.1,minNeighbors=3)
-                for (a,b,c,d) in face_rect:
-                    face_boi=gray[b:b+d,a:a+c]
-                    features.append(face_boi)
-                    labels.append(label)'''
                     
     
 
