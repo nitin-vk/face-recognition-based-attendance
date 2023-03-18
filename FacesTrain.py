5d0fb8b34b37c00f2ce1785f5e7c1b5c0043e126
Nitin V Kavya <nitinvkavya@gmail.com>
2023-03-18 11:33:53 +0530

    added combo box

--- FacesTrain.py
+++ FacesTrain.py
@@ -2,7 +2,7 @@ import os
 import cv2 as cv
 import numpy as np
 from PyQt5 import QtWidgets, uic
-from PyQt5.QtWidgets import QFileDialog
+from PyQt5.QtWidgets import QFileDialog,QMessageBox
 import sys
 import ftplib
 from ftplib import FTP
@@ -18,16 +18,22 @@ class FacesTrain(QtWidgets.QMainWindow):
         self.dir=''
         self.haar_cascade=''
         self.ftp_dir=''
-        self.selectDir.clicked.connect(self.selectDirectory)
+        self.selectClassBtn.clicked.connect(self.selectDirectory)
+        self.trainBtn.setEnabled(False)
         self.trainBtn.clicked.connect(self.trainFaces)
         self.progressFrame.hide()
         self.doneFrame.hide()
 
     
     def selectDirectory(self):
-        self.dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
-        self.ftp_dir=str(self.dir[self.dir.rindex('/')+1:len(self.dir)])
-        
+        #self.dir = str(QFileDialog.getExistingDirectory(self, "Select Directory"))
+        #self.ftp_dir=str(self.dir[self.dir.rindex('/')+1:len(self.dir)])
+        self.ftp_dir=os.path.join("D:/Faces",self.branchComboBox.currentText(),self.yearComboBox.currentText(),self.sectionComboBox.currentText())
+        self.dir=self.ftp_dir
+        if os.path.exists(self.dir)==False:
+            QMessageBox.information(self, "ERROR", "Training Directory Does Not Exsist")
+        else:
+            self.trainBtn.setEnabled(True)
 
     def trainFaces(self):
         if self.dir=='':
@@ -50,9 +56,9 @@ class FacesTrain(QtWidgets.QMainWindow):
                 face_encoding = face_recognition.face_encodings(image)[0]
                 known_face_encodings.append(face_encoding)
         folderName=self.ftp_dir
-        if os.path.exists('D:/Compiled Files/'+folderName)==False:
-            os.mkdir('D:/Compiled Files/'+folderName)
-        with open((os.path.join('D:/Compiled Files/',folderName,'encodings.txt')), "wb") as fp:
+        if os.path.exists(os.path.join("D:/Compiled Files",self.branchComboBox.currentText(),self.yearComboBox.currentText(),self.sectionComboBox.currentText()))==False:
+            os.mkdir(os.path.join("D:/Compiled Files",self.branchComboBox.currentText(),self.yearComboBox.currentText(),self.sectionComboBox.currentText()))
+        with open((os.path.join('D:/Compiled Files/',self.branchComboBox.currentText(),self.yearComboBox.currentText(),self.sectionComboBox.currentText(),'encodings.txt')), "wb") as fp:
             pickle.dump((known_face_encodings, known_face_names), fp)
         self.doneFrame.show()
         
