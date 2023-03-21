7f0f0a3face7497c190784218beef4f79d60ce63
Nitin V Kavya <nitinvkavya@gmail.com>
2023-03-21 18:00:02 +0530

    Removed LBPH

--- Student_ID.py
+++ Student_ID.py
@@ -1,114 +1,121 @@
 from PyQt5 import QtWidgets, uic
-import sys,os
+import sys,os,face_recognition,pickle,numpy as np
 from PyQt5.QtWidgets import QFileDialog,QMessageBox
 import cv2 as cv
 from FaceDetectionModule import FaceDetectionModule
 from tk_id_card_main import TK_ID_CARD_MAIN
+from PIL import Image
 
 class Student_ID(QtWidgets.QMainWindow):
     def __init__(self):
         super(Student_ID, self).__init__()
         uic.loadUi('Student_ID.ui', self)
         self.show()
-        self.createIdButton.setEnabled(False)
-        self.selectCompiledButton.clicked.connect(self.selectCompiledFile)
+        self.submitBtn.clicked.connect(self.selectCompiledFile)
         self.createIdButton.clicked.connect(self.startRecognition)
+        self.photoBtn.clicked.connect(self.takePhoto)
         self.yml_file=''
+        self.capture=''
+        self.idPhoto=''
         self.people=[]
         self.faces_read={}
         
+    def takePhoto(self):
+        self.capture = cv.VideoCapture(0)
+        while True:
+            ret, frame = self.capture.read()
+            self.idPhoto = frame
+            cv.imshow('take it',frame)
+            if cv.waitKey(1) == ord('s'):
+                break
+        self.capture.release()
+        cv.destroyAllWindows()
+        img = Image.fromarray(self.idPhoto)
+        img.save('face.jpg')
+        
 
     def selectCompiledFile(self):
-        self.yml_file=QFileDialog.getOpenFileName(self, 'Open file', 
-        'c:\\',"YML files (*.yml)")
-        print(self.yml_file)
-        self.yml_file=str(self.yml_file)
-        pos=self.yml_file.index(',')
-        self.yml_file=self.yml_file[2:pos-1]
-        '''pos=self.yml_file.index(',')
-        self.yml_file=self.yml_file[2:pos-1]
-        pos=self.yml_file.rindex('/')
-        yml_file=self.yml_file[pos+1:len(self.yml_file)+1]
-        #print(self.yml_file)
-        if self.yml_file!='':
-            self.fileLocation.insertPlainText(yml_file)
-        self.afterSelectFrame.show()
-        self.downloadBtn.show()
-        self.spreadSheetBtn.show()
-        self.downloadBtn.setEnabled(False)
-        self.spreadSheetBtn.setEnabled(False)
-        self.recognizeBtn.show()'''
+        self.yml_file=os.path.join('D:\Compiled Files',self.branchComboBox.currentText(),self.yearComboBox.currentText(),self.sectionComboBox.currentText(),"encodings.txt")
         if self.yml_file!='':
             self.createIdButton.setEnabled(True)
 
     def startRecognition(self):
+        if self.yml_file=="":
+            QMessageBox.warning(self,"error","Select a Compiled File")
+            return
+        if self.idPhoto=="":
+            QMessageBox.warning(self,"error","Click your picture")
+            return
+        unknown_face=False
+        with open(self.yml_file, "rb") as fp:
+            known_face_encodings, known_face_names = pickle.load(fp)
+        self.usn=[]
         self.people=[]
-        self.faces_read={}
+        for i in known_face_names:
+            self.usn.append(i.split('-')[0])
+            self.people.append(i.split('-')[-1])
         videoType=0
-        l=30
-        t=7
-        dir=r"D:\Faces\8CSEB"
-        haar_cascade = cv.CascadeClassifier(r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\haar_cascade_files\data\haarcascades\haarcascade_frontalcatface.xml")
-        face_recognizer = cv.face.LBPHFaceRecognizer_create()
-        face_recognizer.read(self.yml_file)
-        for i in os.listdir(dir):
-            self.people.append(i)
-        print(self.people)
+        face_locations = []
+        face_encodings = []
+        self.faces_read=[]
+        video = cv.VideoCapture(videoType)
+        while True:	
+            check, frame = video.read()
+            small_frame = cv.resize(frame, (0,0), fx=0.5, fy= 0.5)
+            rgb_small_frame = small_frame[:,:,::-1]
+            face_locations = face_recognition.face_locations(rgb_small_frame)
+            face_encodings = face_recognition.face_encodings(rgb_small_frame, face_locations)
+            face_names = []
+            for face_encoding in face_encodings:
+                matches = face_recognition.compare_faces(known_face_encodings, np.array(face_encoding), tolerance = 0.6)
+                face_distances = face_recognition.face_distance(known_face_encodings,face_encoding)	
+                try:
+                    matches = face_recognition.compare_faces(known_face_encodings, np.array(face_encoding), tolerance = 0.6)
+                    face_distances = face_recognition.face_distance(known_face_encodings,face_encoding)
+                    best_match_index = np.argmin(face_distances)
+                    if matches[best_match_index]:
+                        name = known_face_names[best_match_index]
+                        face_names.append(name)
+                        if name not in self.faces_read:
+                            self.faces_read.append(name)
+                except:
+                    pass
 
-        capture=cv.VideoCapture(videoType)
-        f=FaceDetectionModule()
-        while True:
-    
-            isTrue,img=capture.read()
+            if len(face_names) == 0:
+                for (top,right,bottom,left) in face_locations:
+                    top*=2
+                    right*=2
+                    bottom*=2
+                    left*=2
+                    cv.rectangle(frame, (left,top),(right,bottom), (0,0,255), 2)
+                    font = cv.FONT_HERSHEY_DUPLEX
+                    cv.putText(frame, 'Unknown', (left, top), font, 0.8, (0,0,0),1)
+                    unknown_face=True
+            else:
+                for (top,right,bottom,left), name in zip(face_locations, face_names):   
+                    top*=2
+                    right*=2
+                    bottom*=2
+                    left*=2
+                    cv.rectangle(frame, (left,top),(right,bottom), (0,255,0), 2)
+                    font = cv.FONT_HERSHEY_DUPLEX
+                    cv.putText(frame, name, (left, top), font, 0.8, (255,255,255),1)
 
-            img,boxes=f.findFace(img)
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    
+            cv.imshow("Face Recognition Panel",frame)
 
-
-            faces_rect = haar_cascade.detectMultiScale(gray, 1.1, 4)
-
-            for (x,y,w,h) in faces_rect:
-        
-                x1,y1=x+w,y+h
-                faces_roi = gray[y:y+h,x:x+w]
-
-                label, confidence = face_recognizer.predict(faces_roi)
-                print(f'Label = {self.people[label]} with a confidence of {confidence}')
-                cv.putText(img, str(self.people[label]), (20,20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0,255,0), thickness=2)
-                self.faces_read.setdefault(self.people[label],[])
-                if len(self.faces_read[self.people[label]])<10:
-                    self.faces_read[self.people[label]].append(confidence)
-                    self.faces_read[self.people[label]].sort()
-                else:
-                    if confidence<self.faces_read[self.people[label]][9]:
-                        del self.faces_read[self.people[label]][9]
-                        self.faces_read[self.people[label]].append(confidence)
-                        self.faces_read[self.people[label]].sort()
-
-                    
-        
-            cv.imshow('Detected Face', img)
-            if cv.waitKey(20) & 0xFF==ord('b'):
+            if cv.waitKey(1) == ord('s'):
                 break
 
-        capture.release()
+        video.release()
         cv.destroyAllWindows()
-        #print(self.faces_read)
-        for i in self.faces_read.keys():
-            self.faces_read[i]=(sum(self.faces_read[i])/len(self.faces_read[i]))
-        #print(self.faces_read)
-        person_id = min(self.faces_read, key=self.faces_read.get)
-        print(person_id)
-        usn=person_id[0:10]
-        name=person_id[11:len(person_id)]
-        img_path=person_id
-        id_card=TK_ID_CARD_MAIN(usn,name,img_path)
+        self.photoBtn.setEnabled(True)
+        recognized_usn=self.faces_read[0].split('-')[0]
+        recognized_name=self.faces_read[0].split('-')[1]
+        #self.idPhoto.save('face.jpg')
+        id_card=TK_ID_CARD_MAIN(recognized_usn,recognized_name,'face.jpg')
+        
 
-
-
-
-
+    
 app = QtWidgets.QApplication(sys.argv)
 window = Student_ID()
 app.exec_()
--- tk_id_card_main.py
+++ tk_id_card_main.py
@@ -17,26 +17,26 @@ class TK_ID_CARD_MAIN:
         self.usn=usn
         self.name=name
         self.img_path=img_path
-        my_path=os.path.join(r"D:\ID_CARDS",self.img_path+'.pdf')
+        my_path=os.path.join(r"D:\ID_CARDS",usn+name+'.pdf')
         c = canvas.Canvas(my_path,pagesize=(600,300))
         c=my_temp(c) # run the template
-        img_dir=os.path.join(r"C:\Users\Nitin V Kavya\Desktop\College\Final_Year_project\Final_Year\Faces\train",self.img_path)
-        for i in os.listdir(img_dir):
-            real_img=os.path.join(img_dir,i)
-            img=cv.imread(real_img)
-            gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-            faceCascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
-            faces = faceCascade.detectMultiScale(
-            gray,
-            scaleFactor=1.3,
-            minNeighbors=3,
-            minSize=(30, 30)
+        img_dir=img_path
+        
+            
+        img=cv.imread(img_dir)
+        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        faceCascade = cv.CascadeClassifier(cv.data.haarcascades + "haarcascade_frontalface_default.xml")
+        faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
                     )
-            for (x, y, w, h) in faces:
-                cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
-                roi_color = img[y:y + h, x:x + w] 
-                print("[INFO] Object found. Saving locally.") 
-                cv.imwrite('id_face.jpg', roi_color)       
+        for (x, y, w, h) in faces:
+            cv.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 2)
+            roi_color = img[y:y + h, x:x + w] 
+            print("[INFO] Object found. Saving locally.") 
+            cv.imwrite('id_face.jpg', roi_color)       
             c.drawImage('id_face.jpg',2.2*inch,0.7*inch) #Add image
             break
 
