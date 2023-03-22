bf9b5d546232320eb255960a577b2d570f05b219
KiranR-max <kkr1392001@gmail.com>
2023-03-22 14:09:20 +0530

    added css to student.ui

--- Student_ID.py
+++ Student_ID.py
@@ -122,5 +122,70 @@ class Student_ID(QtWidgets.QMainWindow):
 
     
 app = QtWidgets.QApplication(sys.argv)
+style="""
+
+Student_ID{
+background: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1, stop: 0 #1abc9c, stop: 1 #3498db)
+}
+
+QPushButton:open { /* when the button has its menu open */
+    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                      stop: 0 #dadbde, stop: 1 #f6f7fa);
+}
+QPushButton::menu-indicator {
+    image: url(menu_indicator.png);
+    subcontrol-origin: padding;
+    subcontrol-position: bottom right;
+}
+QPushButton::menu-indicator:pressed, QPushButton::menu-indicator:open {
+    position: relative;
+    top: 2px; left: 2px; /* shift the arrow by 2 px */
+}
+QPushButton {
+    border: 2px solid #8f8f91;
+    border-radius:  8px;
+    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                      stop: 0 #f6f7fa, stop: 1 #dadbde);
+    min-width: 80px;
+}
+QPushButton:pressed {
+    background-color: qlineargradient(x1: 0, y1: 0, x2: 0, y2: 1,
+                                      stop: 0 #dadbde, stop: 1 #f6f7fa);
+}
+QPushButton:flat {
+    border: none; /* no border for a flat push button */
+}
+QPushButton:default {
+    border-color: navy; /* make the default button prominent */
+}
+QPushButton:hover{
+        background: qlineargradient(x1 : 0, y1 : 0, x2 : 0, y2 :   1, stop :   0.0 #ffd9aa,
+                stop :   0.5 #ffbb6e, stop :   0.55 #feae42, stop :   1.0 #fedb74);
+}
+QPushButton::menu-indicator {
+    image: url(menu_indicator.png);
+    subcontrol-origin: padding;
+    subcontrol-position: bottom right;
+}
+
+QWidget{
+    background-color:#E0E0E0;
+}
+
+QFrame#frame{
+    border: 2px solid #ccc;
+    background-color: #778899;
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+}
+QFrame#frame_2,#frame_3,#frame_4{
+    border: 2px solid #ccc;
+    background-color: #FFE5E5;
+    font-family: Arial, sans-serif;
+    font-size: 14px;
+    
+}
+"""
+app.setStyleSheet(style)
 window = Student_ID()
 app.exec_()
